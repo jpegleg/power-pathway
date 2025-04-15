5aaa84d6817b52083755febd67f2f7845b0858e6
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2025-04-15 15:14:52 -0400

    Create mortal.ps1

---
+++ scripts/mortal.ps1
@@ -0,0 +1,22 @@
+$PWord = Read-Host -Prompt 'Enter AES256CTR password' -AsSecureString
+$ptr   = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($PWord)
+$pass  = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($ptr)
+
+$Env:TEMPAESP = $pass
+
+Get-Content ./crypt.json -Raw |
+    ConvertFrom-Json |
+    ForEach-Object -Parallel {
+        $path = $_.target
+        $sample = $_.sample
+        $cipher = $_.destination
+        if (Test-Path $path) {
+            "$path => file found, processing..."
+            AES256CTR $cipher $path $sample -ad
+        } else {
+            "$path => file not found, skipping..."
+        }
+    } -ThrottleLimit 10
+
+$env:TMPAESP = $null
+[Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ptr)
